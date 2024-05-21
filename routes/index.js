c3158fb5ea74657353ad9e4a6c1c2d735a1bb844
Sam Schaffer <sfschaffer99@gmail.com>
2024-05-21 15:18:44 -0700

    add posting functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'messageboard', messages:messages});
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('new', {title:"submitform"});
+});
+
+router.post('/new', function(req, res, next) {
+    messages.push({text: req.body.message, user: req.body.user, added: new Date()});
+    res.redirect("/");
 });
 
 module.exports = router;
